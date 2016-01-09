714a1be432f25f8070b1a330a86989f2b1a13938
QwQ <adsl88788190@gmail.com>
2016-01-10 01:09:10 +0800

    New Icon & Action Icon

--- English/app/src/main/java/com/qwq/english/MainActivity.java
+++ English/app/src/main/java/com/qwq/english/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
         setData();
         lv = (ListView) findViewById(R.id.list_view);
         inputSearch = (EditText) findViewById(R.id.inputSearch);
